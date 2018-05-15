d6982fdc08aa90637460a3cd059988d5ff2e2eca
goliath191 <miragliomatias@gmail.com>
2018-05-15 00:07:44 -0300

    Create Special-items Section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
-    
+
+    //Ripples
     $("#header, .info").ripples({
         dropRadius: 25,
-        perturbance: 0.6
-        
+        perturbance: 0.1
+
     });
-    
+
+
+    //Magnific-popup
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
 });
